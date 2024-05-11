2c771b2edbc10a35c7bc8988af6f0ca0fd5446e5
Aisyah Fadzila Hani <aisyahfadzila@student.telkomuniversity.ac.id>
2024-05-11 18:08:43 +0700

    features: add empty state

--- app/src/main/java/org/d3if3044/wanderlist/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3044/wanderlist/ui/screen/MainScreen.kt
@@ -2,6 +2,7 @@ package org.d3if3044.wanderlist.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -33,7 +34,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -121,6 +124,12 @@ fun ScreenContent(showList: Boolean, modifier: Modifier, navController: NavHostC
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.cancel_9462411),
+                contentDescription = stringResource(R.string.empty_state_image),
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.padding(16.dp)
+            )
             Text(text = stringResource(id = R.string.list_kosong))
         }
     } else {
